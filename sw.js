3a86cb4b65f2799e1ee2f8b73b45621611a1bb23
Mohamed Javer <Mohamedarbi0208@gmail.com>
2026-08-14 21:03:04 +0000

    Avatar: cache-bust signed URL on each change + SW bdl-v3

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ self.addEventListener('activate',e=>{
 
 
 /* ===== Web Share Target: استقبال إيصالات الواتساب (POST محلي) ===== */
-const SHARE_CACHE='bdl-v2';
+const SHARE_CACHE='bdl-v3';
 
 async function arkHandleShare(req){
   try{
